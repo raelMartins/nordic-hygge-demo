037757a43afc12929a71e68b939b558bfbb04178
Martins Akeredolu <olamideakeredolu@gmail.com>
2025-05-13 11:24:42 +0100

    test: testing metadata deployment

--- src/pages/listing-details/[id].js
+++ src/pages/listing-details/[id].js
@@ -1,10 +1,89 @@
 import {fetchProjectsByIdServerside} from '@/api/listing';
 import {AuthLayout} from '@/components/page_layout/AuthLayout';
 import {ListingPage} from '@/components/properties/listings/ListingPage';
+import useGetSession from '@/utils/hooks/getSession';
 import {getServersideCookie} from '@/utils/sessionmanagers';
+import Head from 'next/head';
 
 const ListingDetails = ({info, err}) => {
-  return <AuthLayout InnerComponent={<ListingPage info={info} err={err} />} />;
+  const {sessionData: store_data} = useGetSession('store_data');
+
+  return (
+    <>
+      <Head>
+        {/* Basic */}
+        <title>
+          {capitalizeString(
+            metaData?.title
+              ? `${metaData?.title} | ${store_data?.store_name}`
+              : store_data?.store_name
+              ? `${store_data?.store_name} - Luxury Properties for Discerning Buyers`
+              : `loading`
+          )}
+        </title>
+        <meta
+          name="description"
+          content={metaData?.description || `Property Development Company`}
+        />
+
+        {/* Open Graph Meta Tags */}
+        <meta
+          property="og:title"
+          content={capitalizeString(
+            metaData?.title
+              ? `${metaData?.title} | ${store_data?.store_name}`
+              : store_data?.store_name
+              ? `${store_data?.store_name} - Luxury Properties for Discerning Buyers`
+              : `loading`
+          )}
+        />
+        <meta
+          property="og:description"
+          content={metaData?.description || `Property Development Company`}
+        />
+        <meta
+          property="og:image"
+          content={
+            metaData?.image?.photo ||
+            metaData?.image?.original ||
+            metaData?.image ||
+            store_data?.company_image
+          }
+        />
+        <meta property="og:site_name" content={store_data?.store_name} />
+
+        {/* Twitter Meta Tags */}
+        <meta
+          name="twitter:title"
+          content={capitalizeString(
+            metaData?.title
+              ? `${metaData?.title} | ${store_data?.store_name}`
+              : store_data?.store_name
+              ? `${store_data?.store_name} - Luxury Properties for Discerning Buyers`
+              : `loading`
+          )}
+        />
+        <meta
+          name="twitter:description"
+          content={metaData?.description || `Property Development Company`}
+        />
+        <meta
+          name="twitter:image"
+          content={
+            metaData?.image?.photo ||
+            metaData?.image?.photo?.[0] ||
+            metaData?.image?.original ||
+            metaData?.image ||
+            store_data?.company_image
+          }
+        />
+        <meta name="twitter:image:alt" content={`Image of ${store_data?.store_name}`} />
+        <meta name="twitter:site" content={`@${store_data?.store_name}`} />
+        <meta name="twitter:card" content={'summary_large_image'} />
+      </Head>
+      <AuthLayout InnerComponent={<ListingPage info={info} err={err} />} />
+    </>
+  );
 };
 
 // export default Auth(ListingDetails);
